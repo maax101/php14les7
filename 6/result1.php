b65c3764848cc1d616f0c13281e0a9b80a2cdc47
maax101 <belous.maxim@gmail.com>
2017-08-08 16:42:29 +0300

    Add files via upload

--- 6/result1.php
+++ 6/result1.php
@@ -26,10 +26,12 @@ file_put_contents('allresult.php','<br>'. date('d.m.y').'. '.$user.': прави
 $sert = @imagecreatetruecolor(640, 320) or die ('Невозможно инициализировать GD поток');	
 $text_col = imagecolorallocate($sert, 15, 90, 20);
 $fill = imagecolorallocate($sert, 220, 220, 220);
+$font = __DIR__.'/arial.ttf';
+
 imagefill($sert, 10, 10, $fill);
-imagettftext($sert, 22, 0, 200, 50, $text_col, 'arial.ttf', 'Ваш результат, '.$user.':');
-imagettftext($sert, 15,0, 50, 100,$text_col,'arial.ttf', 'правильно: '.$ot);
-imagettftext($sert, 15,0, 50, 150,$text_col,'arial.ttf', 'неправильно: '.$not);
+imagettftext($sert, 22, 0, 200, 50, $text_col, $font, 'Ваш результат, '.$user.':');
+imagettftext($sert, 15,0, 50, 100,$text_col,$font, 'правильно: '.$ot);
+imagettftext($sert, 15,0, 50, 150,$text_col,$font, 'неправильно: '.$not);
 header('Content-Type: image/png; Location: list.php');
 imagepng($sert);
 imagedestroy($sert);
